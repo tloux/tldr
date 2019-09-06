4c92a21bd168ead0303c6646f5f69ded23b8ba4b
tloux <travis.loux@gmail.com>
2019-09-06 15:45:17 -0500

    0.1.7 add cont_compare function

---
+++ R/cont_compare.R
@@ -0,0 +1,67 @@
+
+#' Compare a numerical vairable across levels of a categorical variable
+#' 
+#' \code{cont_compare} gives details about the distribution of a numeric variable ascorss subsets of the dataset
+#' 
+#' @param y A numerical variable
+#' @param grp A catgegorical variable
+#' @param plot Type of plot to produce
+#' @param print_plot Whether plot should be printed to screen. Plot will always be saved as part of output list.
+#' 
+#' @return Returns a list including (1) group-wise summary statistics, (2) an ANOVA decomposition, (3) eta-squared effect size, and (4) a ggplot2 object, if requested.
+#' 
+#' @examples
+#' v1 = rbinom(n=50, size=1, p=0.5)
+#' v2 = rnorm(50)
+#' cat_compare(y=v2, grp=v1, plot='density')
+#' 
+#' @export
+
+cont_compare = function(y, grp, plot=c('density','boxplot','none')){
+  
+  mydat = data.frame(grp=as.character(grp), y)
+  
+  # group-wise summary statistics
+  
+  grps = sort(unique(mydat$grp))
+  
+  sum_tab = t(sapply(grps, function(g) summary(mydat$y[mydat$grp==g])))
+  rownames(sum_tab) = grps
+  
+  # ANOVA
+  
+  decomp = summary(aov(y ~ grp, data=mydat))
+  
+  # eta-squared
+  
+  eta_sq = decomp[[1]]$`Sum Sq`[1] / sum(decomp[[1]]$`Sum Sq`)
+  
+  # plot
+  
+  plot = plot[1]
+  
+  if(plot == 'none'){
+    ret = list(sum_tab, decomp, eta_sq)
+
+  }else{
+    if(plot=='density'){
+      myplot = ggplot(data=mydat, aes(x=y, fill=grp)) + 
+        geom_density(alpha=0.5) + 
+        geom_rug(aes(color=grp)) + 
+        facet_grid(grp ~ .) + 
+        theme_minimal() + 
+        theme(legend.position="none")
+    }
+    
+    if(plot == 'boxplot'){
+      myplot = ggplot(data=mydat, aes(y=y, x=grp, fill=grp)) + 
+        geom_boxplot(alpha=0.5) + 
+        theme_minimal() + 
+        theme(legend.position='none')
+    }
+    
+    ret = list(sum_tab, decomp, eta_sq, myplot)
+  }
+  
+  return(ret)
+}
