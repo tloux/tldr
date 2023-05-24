3e4a8790909f67baaed8fa2e189458ea5a19562f
loux <loux@slu.edu>
2023-05-24 11:02:35 -0500

    0.3.0 added KnitableTableOne
    Minor changes to KreateTableOne to simplify

--- R/KreateTableOne.R
+++ R/KreateTableOne.R
@@ -7,38 +7,40 @@
 #'
 #' \code{KreateTableOne} is a wrapper for \code{tableone::CreateTableOne} which
 #' formats the original plain text table as a data.frame of character columns.
-#' This can be printed in an RMarkdown document in a number of ways, e.g., using
+#' \code{KnitableTableOne} is a wrapper for \code{tableone::print.TableOne} which
+#' allows for more versatility in printing options. The output of both functions
+#' can be printed in an RMarkdown document in a number of ways, e.g., using
 #' \code{knitr::kable}. \code{svyKreateTabeOne} does the same with
-#' \code{tableone::svyCreateTableOne}.
+#' \code{tableone::svyCreateTableOne} for complex survey data.
 #'
-#' This is a very hacky function. If used within an RMarkdown document,
-#' KreateTableOne should be called in a code chunk with \code{results='hide'} to
-#' hide the plain test results printed from \code{tableone::CreateTableOne}. The
-#' resulting data frame should be saved as an object and used in a second code
-#' chunk for formatted printing. Suggestions for improvement are welcomed.
+#' These are very hacky functions. If used within an RMarkdown document,
+#' KreateTableOne and KnitableTableOne should be called in a code chunk with
+#' \code{results='hide'} to hide the plain test results printed from
+#' \code{tableone::CreateTableOne}. The resulting data frame should be saved
+#' as an object and used in a second code chunk for formatted printing.
+#' Suggestions for improvement are welcomed.
 #'
 #' The function is written to work with \code{knitr::kable}, but should be able
 #' to work with other functions such as \code{xtable::xtable}.
 #'
-#' @param x The data set to be passed to the \code{data} parameter of
-#'   \code{tableone::CreateTableOne}
-#' @param printSMD Logical passed to \code{smd} parameter of \code{tableone::CreateTableOne}
-#' @param ... Other parameters to be passed to \code{tableone::CreateTableOne}
+#' @param x A TableOne object created from \code{tableone::CreateTableOne}.
+#' @param ... Parameters to be passed to \code{tableone::CreateTableOne}
+#' (\code{KreateTableOne}) or \code{tableone::print.TableOne} (\code{KnitableTableOne}).
 #'
 #' @return Returns a data frame of character columns.
 #'
-#' @seealso \code{\link[tableone]{CreateTableOne}}
+#' @seealso \code{\link[tableone]{CreateTableOne}} \code{\link[tableone]{print.TableOne}}
 #'
 #' @examples
-#' table1 = KreateTableOne(x=mtcars, strata='am', factorVars='vs')
+#' table1 = KreateTableOne(data=mtcars, strata='am', factorVars='vs')
 #' table1
 #' knitr::kable(table1)
 #'
 #' @export
 
-KreateTableOne = function(x, printSMD = TRUE, ...){
-  t1 = tableone::CreateTableOne(data=x, smd=printSMD, ...)
-  t2 = print(t1, quote=TRUE, ...)
+KreateTableOne = function(...){
+  t1 = tableone::CreateTableOne(...)
+  t2 = print(t1, quote=TRUE)
   rownames(t2) = gsub(pattern='\\"', replacement='', rownames(t2))
   colnames(t2) = gsub(pattern='\\"', replacement='', colnames(t2))
   return(t2)
@@ -47,9 +49,18 @@ KreateTableOne = function(x, printSMD = TRUE, ...){
 
 #' @rdname KreateTableOne
 
-svyKreateTableOne = function(x, ..., printSMD = TRUE){
-  t1 = tableone::svyCreateTableOne(data=x, ...)
-  t2 = print(t1, quote=TRUE, ...)
+svyKreateTableOne = function(...){
+  t1 = tableone::svyCreateTableOne(...)
+  t2 = print(t1, quote=TRUE)
+  rownames(t2) = gsub(pattern='\\"', replacement='', rownames(t2))
+  colnames(t2) = gsub(pattern='\\"', replacement='', colnames(t2))
+  return(t2)
+}
+
+#' @rdname KreateTableOne
+
+KnitableTableOne = function(x, ...){
+  t2 = print(x, quote=TRUE, ...)
   rownames(t2) = gsub(pattern='\\"', replacement='', rownames(t2))
   colnames(t2) = gsub(pattern='\\"', replacement='', colnames(t2))
   return(t2)
--- cran-comments.md
+++ cran-comments.md
@@ -1,15 +1,10 @@
 ## Test environments
 
-* local Windows 10 install, R 4.0.2
+* local Windows 10 install, R 4.0.3
 * win-builder
 * rhub
 
 
 ## R CMD check results
 
-There were no ERRORs or WARNINGs.
-
-There are 2 NOTEs.
-
-* One is due to this being a new package submission
-* One is highlighting the possible mis-spelling of my last name. It is correctly spelled.
+There were no ERRORs or WARNINGs or NOTEs.
